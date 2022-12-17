b12a9b5e1773368d55571a7d3e67d484015260d4
RTKmoko <rtkmoko@gmail.com>
2022-12-17 13:56:06 +0200

    made it work

---
+++ entry.py
@@ -0,0 +1,144 @@
+import json
+import rich
+
+
+
+class EntryUI():
+    
+    DATA_FILE_NAME = 'data.json'
+        
+    @staticmethod
+    def read_data() -> dict:
+        with open(EntryUI.DATA_FILE_NAME,'r') as f:
+            return json.load(f)
+
+    @staticmethod
+    def save_data(data: dict):
+        with open(EntryUI.DATA_FILE_NAME,'w') as f:
+            return json.dump(data ,f, indent=4)
+
+    def __init__(self) -> None:
+        self.data = EntryUI.read_data()
+        self.main()
+    
+    def save(self):
+        EntryUI.save_data(self.data)
+    
+    def print_existing(self):
+        idx = 1
+        print()
+        print("[+] Existing substances:")
+        for k,v in self.data.items():
+            print(f'➡  {k.capitalize()}:')
+            for subs in v:
+                name = subs['name']
+                enabled = subs['enabled']
+                print(f'\t{idx} - {"✔" if enabled else "✖"} - {name}')
+                idx += 1
+
+    def list_data(self):
+        array = []
+        for k,v in self.data.items():
+            for substance in v:
+                array.append(substance)
+        return array
+
+    def add_substance(self):
+        print("[+] Adding new substance")
+
+        c_type = Prompt.ask("Select type: [P] for Polymer, [S] for Solvent")
+        if c_type.lower() not in ['p', 's']:
+            exit()
+        _type = 'polymer' if c_type.lower() == 'p' else 'solvent'
+
+        substanceData = {
+            'name': Prompt.ask("Enter name for new substance"),
+            'enabled': True,
+            'd': FloatPrompt.ask("Enter D (decimal)"),
+            'p': FloatPrompt.ask("Enter P (decimal)"),
+            'h': FloatPrompt.ask("Enter H (decimal)"),
+        }
+        if _type == 'polymer':
+            substanceData.update({
+                'r': FloatPrompt.ask("Enter R (decimal)"),
+            })
+
+        self.data[_type].append(substanceData)
+        self.save()
+
+    
+    def delete_substance(self, index):
+        for k,v in self.data.items():
+            if len(v) <= index:
+                index -= len(v)
+                continue
+            del self.data[k][index]
+            break
+        self.save()
+    
+    def toggle_substance(self, index):
+        for k,v in self.data.items():
+            if len(v) <= index:
+                index -= len(v)
+                continue
+            self.data[k][index]['enabled'] = not self.data[k][index]['enabled']
+            break
+        self.save()
+    
+    def main(self):
+        # show data in a list, separated by the type
+        # let user add or select any one from list
+        # when selected can be enabled/disabled or deleted
+        
+        # rich.print_json(json.dumps(self.data)) # DEBUG
+        
+        self.print_existing()
+        
+        data_listing = self.list_data()
+        try:
+            choice = Prompt.ask(f"[A] to add, [1-{len(data_listing)}] to select a substance, [X] to exit", default="x")
+        except:
+            exit()
+        
+        # print('you chose: ', choice)
+        
+        if choice.lower() == 'a':
+            # Add
+            return self.add_substance()
+        elif choice.lower() == 'x':
+            exit()
+        
+        # Try parse to int
+        try:
+            choice = int(choice)
+            if choice < 0 or choice > len(data_listing):
+                print("Out of range")
+            idx = choice -1 
+
+            # Show info about the selected substance
+            subs = data_listing[idx]
+            try:
+                radius = f'\n\tR: {subs["r"]}'
+            except:
+                radius = ''
+
+            print(f"\n[>] Selected substance is: '{subs['name']}'\n" + 
+                  f"\tEnabled?: {'ENABLED' if subs['enabled'] else 'DISABLED'}\n" +
+                  f"\tD: {subs['d']}\n" +
+                  f"\tP: {subs['p']}\n" +
+                  f"\tH: {subs['h']}" + radius)
+            
+            _c2 = Prompt.ask("[T] To toggle (enabled/disabled) , [D] to delete")
+            if _c2.lower() == 't':
+                # Toggle
+                self.toggle_substance(idx)
+            elif _c2.lower() == 'd':
+                # Delete
+                self.delete_substance(idx)
+        except:
+            exit()
+
+from rich.prompt import Prompt, FloatPrompt
+
+if __name__ == '__main__':
+    EntryUI()
--- main.py
+++ main.py
@@ -2,56 +2,45 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 import math as m
+import json
+from pathlib import Path
 
-SPH_D_PARAMETER = float(input('Enter D of the sphere: '))
-SPH_P_PARAMETER = float(input('Enter P of the sphere: '))
-SPH_H_PARAMETER = float(input('Enter H of the sphere: '))
-SPH_R_PARAMETER = float(input('Enter R of the sphere: '))
-
-S_D_PARAM = float(input('Enter D of the solvent: '))
-S_P_PARAM = float(input('Enter P of the solvent: '))
-S_H_PARAM = float(input('Enter H of the solvent: '))
+DATA_FILE = Path('data.json')
 
 
-def cart2sph(x,y,z):
-    XsqPlusYsq = x**2 + y**2
-    r = m.sqrt(XsqPlusYsq + z**2)               # r
-    elev = m.atan2(z,m.sqrt(XsqPlusYsq))     # theta
-    az = m.atan2(y,x)                           # phi
-    return r, elev, az
+def load_data():
+    return json.loads(DATA_FILE.read_text())
 
-def cart2sphA(pts):
-    return np.array([cart2sph(x,y,z) for x,y,z in pts])
 
-def appendSpherical(xyz):
-    np.hstack((xyz, cart2sphA(xyz)))
-    
-# Sphere
-def sphere(SPH_D_PARAMETER, SPH_P_PARAMETER, SPH_H_PARAMETER, SPH_R_PARAMETER):
+def main():
     plt.rcParams["figure.figsize"] = [7.00, 3.50]
     plt.rcParams["figure.autolayout"] = True
+
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
-    # a,b,c = cart2sph(SPH_R_PARAMETER,1,1)
-    r = SPH_R_PARAMETER #radius
-    u, v = np.mgrid[0:2 *np.pi:1000j,0:np.pi:1000j]
+    u, v = np.mgrid[0:2*np.pi:1000j, 0:np.pi:1000j]
     # np.mgrid[b:2 *np.pi:30j,c:np.pi:20j]
     # np.mgrid[0:2 * np.pi:30j, 0:np.pi:20j]
-    # D = 19.1
-    # P = 7.9
-    # H = 9.3
 
-    D = r*(np.cos(u) * np.sin(v))+SPH_D_PARAMETER
-    P = r*(np.sin(u) * np.sin(v))+SPH_P_PARAMETER
-    H = r*(np.cos(v))+SPH_H_PARAMETER
-    ax.plot_surface(D, P, H, cmap='gray',alpha=0.3)
-    #single point
-    def single_point(S_D_PARAM,S_P_PARAM,S_H_PARAM):
-        ax.scatter(S_D_PARAM,S_P_PARAM,S_H_PARAM)
-    
-    
-    single_point(S_D_PARAM,S_P_PARAM,S_H_PARAM)
-    
+    data = load_data()
+
+    for poly in data['polymer']:
+        if not poly['enabled']:
+            continue
+        # Sphere
+        r = poly['r']
+        D = r*(np.cos(u) * np.sin(v))+poly['d']
+        P = r*(np.sin(u) * np.sin(v))+poly['p']
+        H = r*(np.cos(v))+poly['h']
+        ax.plot_surface(D, P, H, cmap='gray', alpha=0.4)
+
+    for sol in data['solvent']:
+        if not sol['enabled']:
+            continue
+        # Single point
+        ax.scatter(sol['d'], sol['p'], sol['h'])
     plt.show()
-    
-sphere(SPH_D_PARAMETER, SPH_P_PARAMETER, SPH_H_PARAMETER, SPH_R_PARAMETER)
+
+
+if __name__ == '__main__':
+    main()
