4275274a6cdd9d2762fc0de09af4ce1bfcdd1543
Ronald Oguz <rtkmoko@gmail.com>
2024-06-14 12:50:25 +0300

    auto version(beginning)

--- version_info.py
+++ version_info.py
@@ -27,19 +27,47 @@ class VersionInfo:
         Import version file from path: self._version_file using importlib.util
         :return: version string in format int,int,int
         """
+        if not os.path.isfile(self._version_file):
+            raise FileNotFoundError(f"Version file not found: {self._version_file}")
+
         f_path, f_name = os.path.split(self._version_file)
         module_name, _ = os.path.splitext(f_name)
-        return ''
 
-    def set(self, version_level: VersionLevel, direction: Direction, step: int):
+        spec = importlib.util.spec_from_file_location(module_name, self._version_file)
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+
+        if hasattr(module, 'version'):
+            version = module.version
+            if isinstance(version, tuple) and all(isinstance(i, int) for i in version):
+                return f'{version}'
+            else:
+                raise ValueError("Version information is not in the correct format (int, int, int)")
+        else:
+            raise AttributeError("Module does not contain 'version' attribute")
+
+    def set(self, version_lvl: VersionLevel, direct_ion: Direction, step1: int):
         """
         Modify current version
-        :param version_level: major, minor, patch
-        :param direction: use operator
-        :param step: count of version point
+        :param version_lvl: major, minor, patch
+        :param direct_ion: use operator
+        :param step1: count of version point
         :return:
         """
-        return ''
+        version_list = list(self._current_version)
+        level_index = version_lvl.value
+        operator_func = direct_ion.value
+
+        # Update the version number at the specified level
+        version_list[level_index] = operator_func(version_list[level_index], int(step1))
+
+        # Ensure no negative version numbers
+        if version_list[level_index] < 0:
+            raise ValueError("Version level cannot be negative")
+
+        # Return the updated version string
+        self._current_version = tuple(version_list)
+        return ','.join(map(str, self._current_version))
 
 
 if __name__ == '__main__':
