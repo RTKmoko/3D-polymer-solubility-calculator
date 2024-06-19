7c52c4da35b52d3a4751510853750a06aaae867b
dmitry.oguz <dmitry.oguz@xmcyber.com>
2024-06-19 21:32:39 +0300

    version_info.py: fixes on Roni; readme_template.md: mv as template

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 from setuptools import setup, find_packages
+from polimer_solibity_calculator.version import VERSION
+
+with open('readme_template.md', 'r') as readme:
+    README = ''.join(readme.readlines())
+    README = README.replace('<version>', VERSION)
+
+with open('README.md', 'w+') as w_help:
+    w_help.write(README)
 
 setup(
     name='pcs_3d',
-    version='1.0.0',
+    version=VERSION,
     packages=find_packages(exclude=['.venv']),
     url='',
     license='',
     author='ronald',
     author_email='',
-    description='',
+    description=README,
     package_data={
         'polimer_solibity_calculator': [
             "scripts/*.sh",
