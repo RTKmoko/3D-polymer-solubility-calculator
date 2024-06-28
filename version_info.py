ba98960d0a9292f0c5900a32ea4bd48255c54c5a
dmitry.oguz <dmitry.oguz@xmcyber.com>
2024-06-28 20:03:18 +0300

    version_info.py: last fix

--- version_info.py
+++ version_info.py
@@ -38,13 +38,10 @@ class VersionInfo:
 
         with open(self._version_file, 'r') as v_file:
             v_file_str = ''.join(v_file.readlines()).strip()
-            print(f"File: {v_file_str}")
-            # ver_name, ver_str = re.split(r"\s*=\s*", v_file_str, 2)
-            # major, minor, patch = [int(i) for i in ver_str[1:-1].split('.')]
+            ver_name, ver_str = re.split(r"\s*=\s*", v_file_str, 2)
+            major, minor, patch = [int(i) for i in ver_str[1:-1].split('.')]
 
-        # self._current_version = major, minor, patch
-        self._current_version = 1, 1, 1
-
+        self._current_version = major, minor, patch
 
     @property
     def version(self):
