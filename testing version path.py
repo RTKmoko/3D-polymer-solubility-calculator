ac3b871ade14db5284b59554d10bff3d61d1a145
Ronald Oguz <rtkmoko@gmail.com>
2024-06-28 18:17:01 +0300

    clean up

--- testing version path.py
+++
@@ -1,8 +0,0 @@
-import os
-
-version_file = 'C:\\Users\\rtkmo\\Documents\\3D-polymer-solubility-calculator\\polimer_solibity_calculator\\version.py'
-
-if os.path.isfile(version_file):
-    print("Version file exists.")
-else:
-    print("Version file does not exist.")
