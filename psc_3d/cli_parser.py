4bbfd4d06f38fdb5ced131d3e67f56f87492a791
RTKmoko <rtkmoko@gmail.com>
2024-06-29 21:38:52 +0300

    cli_parser.py: fix version

--- psc_3d/cli_parser.py
+++ psc_3d/cli_parser.py
@@ -12,12 +12,6 @@ __doc__ = """
 from psc_3d.version import VERSION
 
 
-class ShowVersion(argparse.Action):
-    def __call__(self, parser, namespace, values, option_string=None):
-        print(f"Version: {VERSION}")
-        sys.exit(1)
-
-
 def parser(argv: list = None):
     parser_ = ap.ArgumentParser(description=__doc__)
     parser_.add_argument("-P", "--polymer_type", type=str, required=True, nargs='+',
@@ -29,6 +23,7 @@ def parser(argv: list = None):
     parser_.add_argument("-D", "--data_file", type=str, default='./data.json',
                          help="Data source"
                          )
-    parser_.add_argument('--version', action=ShowVersion)
+    parser_.add_argument('--version', '-v', action='version', version=VERSION)
+
     return parser_.parse_args(argv if argv else sys.argv[1:])
 
